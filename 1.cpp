9d43fe4cda8ffab19facb6564bcfeab507d800c7
unknown <429195-24>
2024-09-17 16:27:56 +0300

    msg

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+#include <math>
+=======
+#include <iostream>
+>>>>>>> b2
+
+using namespace std;
+
+int main(){
+<<<<<<< HEAD
+    c = 1;
+    d = 2;
+    x = c + d;
+=======
+    cout << "hello world!!!!" << endl;
+>>>>>>> b2
+}
